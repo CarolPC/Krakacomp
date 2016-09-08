65615b7ab323b9121fd30b4d4e4c86a3a58e8936
rich <rich@RichMachine>
2016-09-08 16:34:18 -0300

    first commit

---
+++ krakatoa/src/ast/KraClass.java
@@ -0,0 +1,32 @@
+package ast;
+/*
+ * Krakatoa Class
+ */
+public class KraClass extends Type {
+	
+   public KraClass( String name ) {
+      super(name);
+   }
+   
+   public String getCname() {
+      return getName();
+   }
+   
+   private String name;
+   private KraClass superclass;
+   private InstanceVariableList instanceVariableList;
+   // private MethodList publicMethodList, privateMethodList;
+   // m�todos p�blicos get e set para obter e iniciar as vari�veis acima,
+   // entre outros m�todos
+   
+   public KraClass getSuperclass() {
+	   return this.superclass;
+   }
+   
+   public void setSuperclass(KraClass superclass) {
+	   this.superclass = superclass;
+   }
+   public void setInstanceVariableList(InstanceVariableList instanceVariableList) {
+	   this.instanceVariableList = instanceVariableList;
+   }
+}
